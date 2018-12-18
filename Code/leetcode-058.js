e83d49d7d64c486a9063df60b1bb86c58b086e47
zhaoyu <914888156@qq.com>
2018-12-18 15:04:13 +0800

    update

--- Code/leetcode-058.js
+++ Code/leetcode-058.js
@@ -7,10 +7,6 @@
 // Input: 'a '               output:1
 var lengthOfLastWord = function(s) {
     // 用.trim去掉头尾空格 ，然后拆分成数组
-    var strArr = s.trim().split(' ');
-    if(strArr.length < 1){
-        return 0;
-    }
-    var last = strArr.length -1;   
-    return strArr[last].length;  
+    var strArr = s.trim().split(' '); 
+    return strArr[strArr.length -1].length; 
 };
